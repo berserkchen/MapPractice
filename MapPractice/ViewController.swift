84ebf3ee1b1d92d57bcaa5419ee57df70578524d
berserkchen <simson@netease.com>
2014-11-25 14:56:42 +0800

    Initial Commit

---
+++ MapPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapPractice
+//
+//  Created by Jian Chen on 14/11/25.
+//  Copyright (c) 2014年 forall.com.cn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
