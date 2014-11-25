3740eb8b89778f330076a09a4430798f4a2cf6fc
berserkchen <simson@netease.com>
2014-11-25 15:28:20 +0800

    t

--- MapPractice/ViewController.swift
+++ MapPractice/ViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet var myMap : MKMapView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +24,41 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func zoomToNewYork(){
+        
+        var latitude:CLLocationDegrees = 40.75
+        var longtitude:CLLocationDegrees = -73.99
+        
+        var latitudeDelta: CLLocationDegrees = 0.01
+        var longtitudeDelta: CLLocationDegrees = 0.01
+        
+        var mapSpan:MKCoordinateSpan = MKCoordinateSpanMake(latitude, longtitude)
+        
+        var myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longtitude)
+        
+        var myRegion:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, mapSpan)
+        
+        myMap.setRegion(myRegion, animated: true)
+        
+        var empireStatePin = MKPointAnnotation()
+        
+        empireStatePin.title = "Empire State Building"
+        empireStatePin.subtitle = "Once the tallest building"
+        empireStatePin.coordinate = myLocation
+        
+        myMap.addAnnotation(empireStatePin)
+        
+    }
+    @IBAction func satelliteView(){
+        
+        myMap.mapType = MKMapType.Satellite
+        
+    }
+    @IBAction func normalView(){
+        myMap.mapType = MKMapType.Standard
+        
     }
 
 
